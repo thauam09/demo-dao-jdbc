9a54d69bb23241aa5bc643670ff9ac1a81545497
unknown <thauam_09martins@hotmail.com>
2021-09-29 00:02:16 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,16 +2,20 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj= new Department(1, "Books");
+		Department obj = new Department(1, "Books");
 		System.out.println(obj);
-		
+
 		Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 2000.0, obj);
+
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println(seller);
 	}
 
