dbd610b5600737d01ec00d21c69b101771620f5d
unknown <thauam_09martins@hotmail.com>
2021-09-30 18:27:59 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -28,6 +31,23 @@ public class Program {
 		for(Seller obj: sellers) {
 			System.out.println(obj);			
 		}
+		
+		System.out.println("\n=== TEST 4: Seller Insert ===");
+		Seller newSeller = new Seller();
+		newSeller.setName("Joãozinho");
+		newSeller.setEmail("joazinho@gmail.com");
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		Date birthDate = new Date();
+		try {
+			birthDate = dateFormat.parse("25/12/2000");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		newSeller.setBirthDate(birthDate);
+		newSeller.setBaseSalary(4500.0);
+		newSeller.setDepartment(department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -25,7 +26,35 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+
+		try {
+			st = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES (?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if(rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+				DB.closeResultSet(rs);
+			} else {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 
 	}
 
@@ -92,24 +121,23 @@ public class SellerDaoJDBC implements SellerDao {
 		ResultSet rs = null;
 
 		try {
-			st = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
-							+ "ON seller.DepartmentId = department.Id ORDER BY Name");
+			st = conn.prepareStatement("SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department " + "ON seller.DepartmentId = department.Id ORDER BY Name");
 			rs = st.executeQuery();
 			List<Seller> sellers = new ArrayList<Seller>();
 			Map<Integer, Department> map = new HashMap<Integer, Department>();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				Department dep = map.get(rs.getInt("DepartmentId"));
-				
-				if(dep == null) {
+
+				if (dep == null) {
 					dep = instantiateDepartment(rs);
 				}
-				
-				Seller seller = instantiateSeller(rs, dep);	
+
+				Seller seller = instantiateSeller(rs, dep);
 				sellers.add(seller);
 			}
-			
+
 			return sellers;
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
@@ -132,18 +160,18 @@ public class SellerDaoJDBC implements SellerDao {
 			rs = st.executeQuery();
 			List<Seller> sellers = new ArrayList<Seller>();
 			Map<Integer, Department> map = new HashMap<Integer, Department>();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				Department dep = map.get(rs.getInt("DepartmentId"));
-				
-				if(dep == null) {
+
+				if (dep == null) {
 					dep = instantiateDepartment(rs);
 				}
-				
-				Seller seller = instantiateSeller(rs, dep);	
+
+				Seller seller = instantiateSeller(rs, dep);
 				sellers.add(seller);
 			}
-			
+
 			return sellers;
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
